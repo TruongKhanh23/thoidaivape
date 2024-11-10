9e2eea528e107cfc9e9f0c3b86dd6540c99b0b92
Truong Khanh <truongnguyenkhanh230800@gmail.com>
2024-11-10 14:42:02 +0700

    TDV-005: Install Font Awesome Icon

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FontAwesomeIcon from './fontAwesome'
 
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(store)
 app.use(router)
 
