054f129966713a2a9f7fe0f15f55de019ca1301c
Truong Khanh <truongnguyenkhanh230800@gmail.com>
2024-11-10 15:48:25 +0700

    TDV-007: Header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="flex justify-center items-center p-4 w-full">
-    <div class="grid grid-cols-3 items-center w-[60%]">
+    <div class="grid grid-cols-3 items-center w-[100%] md:w-[100%] lg:w-[70%]">
       <!-- Column 1: Search Component -->
       <div class="flex justify-start">
-        <Search />
+        <!-- Hiển thị SearchMobile trên mobile -->
+        <SearchMobile v-if="isSearchVisible && isMobile" @closeSearch="closeSearch" />
+
+        <!-- Hiển thị Search trên desktop -->
+        <Search v-if="!isSearchVisible && !isMobile" />
+
+        <!-- Hiển thị icon tìm kiếm nếu không có vùng nhập nội dung -->
+        <button v-if="!isSearchVisible && isMobile" @click="openSearch" class="p-2 text-xl">
+          <font-awesome-icon icon="search" />
+        </button>
       </div>
 
       <!-- Column 2: Logo Component (Centered) -->
@@ -25,8 +34,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import SearchMobile from './SearchMobile.vue'
 import Search from './Search.vue'
 import Logo from './Logo.vue'
+
+const isSearchVisible = ref(false) // Define the state of the search visibility
+const isMobile = ref(window.innerWidth < 1024) // Check if the screen width is mobile
+
+// Hàm mở tìm kiếm
+function openSearch() {
+  isSearchVisible.value = true
+}
+
+// Hàm đóng tìm kiếm
+function closeSearch() {
+  isSearchVisible.value = false
+}
 
 function goToLogin() {
   window.location.href = '/login'
