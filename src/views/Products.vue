b8cf3fbdb0ac178cb5a2c71782e03d6c7b4dd8e5
Truong Khanh <truongnguyenkhanh230800@gmail.com>
2024-11-11 20:57:42 +0700

    TDV-013: Structure Project

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="flex justify-center bg-[#EAEBED] min-h-screen pt-2 pb-4">
-    <p>Products</p>
+    <p>Products: {{ productId }}</p>
   </div>
 </template>
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const productId = computed(() => route.params.id)
+</script>
