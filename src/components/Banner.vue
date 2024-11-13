9e7299c7fadb4da1c4677898f84e6676a4a6fa35
Trương Khánh <truongn.khanh@dai-ichi-life.com.vn>
2024-11-13 18:08:09 +0700

    TDV-023: Banner.vue - Fix mobile responsive

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-wrap lg:flex-nowrap gap-4 mx-auto w-full">
-      <!-- Banner item 1 -->
+    <div class="flex flex-col lg:flex-row gap-4 w-full mx-auto">
+      <!-- Cột 1 (Banner ULTRA SALE) chiếm 50% chiều rộng -->
       <div
         class="flex items-center justify-center rounded-lg w-full lg:w-1/2 h-48 bg-gradient-to-r from-orange-400 to-yellow-500"
         @click="navigateToProducts"
@@ -19,42 +19,47 @@
         </template>
       </div>
   
-      <!-- Banner item 2 -->
-      <div
-        class="flex items-center justify-center rounded-lg shadow-lg  w-full lg:w-1/4 h-48 bg-white"
-        @click="navigateToProducts"
-      >
-        <template v-if="banners[1].image">
-          <img
-            :src="banners[1].image"
-            alt="Banner 2"
-            class="object-cover w-full h-full rounded-lg"
-          />
-        </template>
-        <template v-else>
-          <div class="flex items-center justify-center w-full h-full text-gray-800 font-bold text-2xl">
-            12.12
+      <!-- Cột 2 (Chứa Banner 2 và Banner 3) chiếm 50% chiều rộng -->
+      <div class="flex flex-col gap-4 w-full lg:w-1/2">
+        <div class="flex gap-4 w-full">
+          <!-- Banner item 2 -->
+          <div
+            class="flex items-center justify-center rounded-lg shadow-lg w-1/2 h-48 bg-white"
+            @click="navigateToProducts"
+          >
+            <template v-if="banners[1].image">
+              <img
+                :src="banners[1].image"
+                alt="Banner 2"
+                class="object-cover w-full h-full rounded-lg"
+              />
+            </template>
+            <template v-else>
+              <div class="flex items-center justify-center w-full h-full text-gray-800 font-bold text-2xl">
+                12.12
+              </div>
+            </template>
           </div>
-        </template>
-      </div>
   
-      <!-- Banner item 3 -->
-      <div
-        class="flex items-center justify-center rounded-lg w-full lg:w-1/4 h-48 bg-gradient-to-r from-orange-400 to-yellow-500"
-        @click="navigateToProducts"
-      >
-        <template v-if="banners[2].image">
-          <img
-            :src="banners[2].image"
-            alt="Banner 3"
-            class="object-cover w-full h-full rounded-lg"
-          />
-        </template>
-        <template v-else>
-          <div class="flex items-center justify-center w-full h-full text-white font-bold text-2xl text-center">
-            CHỈ CÓ TẠI <br />THỜI ĐẠI VAPE
+          <!-- Banner item 3 -->
+          <div
+            class="flex items-center justify-center rounded-lg w-1/2 h-48 bg-gradient-to-r from-orange-400 to-yellow-500"
+            @click="navigateToProducts"
+          >
+            <template v-if="banners[2].image">
+              <img
+                :src="banners[2].image"
+                alt="Banner 3"
+                class="object-cover w-full h-full rounded-lg"
+              />
+            </template>
+            <template v-else>
+              <div class="flex items-center justify-center w-full h-full text-white font-bold text-2xl text-center">
+                CHỈ CÓ TẠI <br />THỜI ĐẠI VAPE
+              </div>
+            </template>
           </div>
-        </template>
+        </div>
       </div>
     </div>
   </template>
